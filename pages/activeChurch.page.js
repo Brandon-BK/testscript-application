29ba80a2d4045ac547b352cf01747e2b307f7c54
Brandon <kisibubrandon5@gmail.com>
2022-06-24 14:36:33 +0200

    active church filling form

--- pages/activeChurch.page.js
+++ pages/activeChurch.page.js
@@ -1,10 +1,27 @@
 class  ActiveChurch{
 
-   get giveYourLifeBtn() { return $('.open-button') }
+   //selectors
+   get giveYourLifeBtn()         { return $('//button[contains(text(), "Give your life")]') }
+   get enterName()               { return $('input[placeholder="Enter Name"]') }
+   get enterEmail()              { return $('input[placeholder="Enter Email"]') }
 
+
+   //functions
    async clickGiveYourLifeButton() {
     await this.giveYourLifeBtn.waitForDisplayed()
     await this.giveYourLifeBtn.click()
+   }
+
+
+   async setNameValue(text) {
+      await this.enterName.waitForDisplayed()
+      await this.enterName.setInputValue(text)
+   }
+
+
+   async setEmailValue(text) {
+      await this.enterName.waitForDisplayed()
+      await this.enterName.setInputValue(text)
    }
     
 }
